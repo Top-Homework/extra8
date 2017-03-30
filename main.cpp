ac233a5ad9d64306662ea587dbd09d37ed7f40e2
lcarranco <lcarranco@gmail.com>
2017-03-30 18:51:46 -0500

    Created a linked list

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <cstdio>
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
 struct node {
-    char data;
-    node *left;
-    node *right;
+    char data = ' ';
+    node *left = 0;
+    node *right = 0;
+};
+
+class tree {
+public:
+    tree(){}
+
+    ~tree(){}
+
+    // void print(list<tree> const & list) {
+    //     node *temp = root;
+    //     for (int i = 0; i < list.size(); i++) {
+    //         cout << temp->data << endl;
+    //         temp = temp->left;
+    //     }
+    // }
+
+private:
+    node *root = 0;
+    
 };
 
 void preorder(node *root) {
@@ -17,7 +39,13 @@ void preorder(node *root) {
     preorder(root->right);
 }
 
+
 int main(int argc, char * argv[]) {
-    preoder();
+    // preorder();
+    list<char> tree;
+    ostream_iterator<int> screen(cout, " ");
+    tree.push_back('A');
+    copy(tree.begin(), tree.end(), screen);
+    cout << endl;
     return 0;
 }
