ff8ba7811a7eecbb174dda322e46e5f7bf4b89ed
lcarranco <lcarranco@gmail.com>
2017-04-01 10:08:57 -0500

    Added print function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <cstdio>
 #include <iostream>
 #include <list>
-#include <algorithm>
-#include <iterator>
 
 using namespace std;
 
@@ -18,22 +16,24 @@ public:
 
     ~tree(){}
 
-    // void print(list<tree> const & list) {
-    //     node *temp = root;
-    //     for (int i = 0; i < list.size(); i++) {
-    //         cout << temp->data << endl;
-    //         temp = temp->left;
-    //     }
-    // }
 
 private:
     node *root = 0;
     
 };
 
+void print(list<char> & mylist) {
+    cout << "\ttree contains:";
+    for (list<char>::iterator it = mylist.begin(); it != mylist.end(); ++it) {
+        cout << " " << *it;
+    }
+    cout << endl;
+}
+
 void preorder(node *root) {
-    if (root == nullptr)
+    if (root == nullptr) {
         return;
+    }
     cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
@@ -42,10 +42,16 @@ void preorder(node *root) {
 
 int main(int argc, char * argv[]) {
     // preorder();
-    list<char> tree;
-    ostream_iterator<int> screen(cout, " ");
-    tree.push_back('A');
-    copy(tree.begin(), tree.end(), screen);
+    list<char> mylist;
+    tree a;
+    mylist.push_back('A');
+    mylist.push_back('B');
+    mylist.push_back('C');
+    mylist.push_back('D');
+
+    print(mylist);
+    // a.print(mylist);
+    
     cout << endl;
     return 0;
 }
